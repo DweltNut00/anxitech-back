f621f87e38a93c75f889f68df9296f7cdb375426
DweltNut00 <ejhandrade2002@gmail.com>
2026-04-17 16:52:05 -0600

    cors

--- api/auth.php
+++ api/auth.php
@@ -3,10 +3,6 @@ require_once __DIR__ . '/../config/cors.php';
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../controllers/AuthController.php';
 
-require_once 'headers.php';
-require_once '..\config\db.php';
-require_once '..\controllers\AuthController.php';
-
 $action = $_GET['action'] ?? '';
 $data = json_decode(file_get_contents('php://input'), true);
 
