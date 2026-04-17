bde12525348256689b60d3fa8c23324a4e8512b2
DweltNut00 <ejhandrade2002@gmail.com>
2026-04-17 16:57:14 -0600

    cors

--- api/headers.php
+++ api/headers.php
@@ -1,4 +1,21 @@
 <?php
+$allowed_origins = [
+    'https://anxitechfrontend.netlify.app',
+    'http://localhost:5173',
+];
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 header("Access-Control-Allow-Origin: *"); // Permite que el frontend acceda a la API
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
