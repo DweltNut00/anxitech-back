85d31c7ca4b96cc32cd137cf9de88c1c61f5de62
DweltNut00 <ejhandrade2002@gmail.com>
2026-04-17 20:46:36 -0600

    cors

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,54 +1,51 @@
 <?php
-require_once __DIR__ . '/../config/cors.php';
-require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../models/Usuario.php';
 
 class AuthController {
     public function login($username, $password) {
-        global $pdo; // Usa la variable global $pdo
-        $user = new Usuario($pdo); // Pasa el objeto PDO al constructor de la clase Usuario
+        global $pdo;
+        $user = new Usuario($pdo);
         return $user->login($username, $password);
     }
 
     public function validateEmail($email) {
-        global $pdo; // Usa la variable global $pdo
-        $user = new Usuario($pdo); // Pasa el objeto PDO al constructor de la clase User
+        global $pdo;
+        $user = new Usuario($pdo);
         return $user->validateEmail($email);
     }
 
     public function validateUsername($username) {
-        global $pdo; // Usa la variable global $pdo
-        $user = new Usuario($pdo); // Pasa el objeto PDO al constructor de la clase User
+        global $pdo;
+        $user = new Usuario($pdo);
         return $user->validateUsername($username);
     }
 
     public function validateNoControl($nocontrol) {
-    global $pdo;
-    $user = new Usuario($pdo);
-    return $user->validateNoControl($nocontrol);
+        global $pdo;
+        $user = new Usuario($pdo);
+        return $user->validateNoControl($nocontrol);
     }
 
     public function register($username, $nombre, $apellido, $email, $password, $nocontrol, $sexo, $fechan, $estadoc, $ciudad, $estado) {
-        global $pdo; // Usa la variable global $pdo
-        $user = new Usuario($pdo); // Pasa el objeto PDO al constructor de la clase User
+        global $pdo;
+        $user = new Usuario($pdo);
         return $user->register($username, $nombre, $apellido, $email, $password, $nocontrol, $sexo, $fechan, $estadoc, $ciudad, $estado);
     }
 
     public function registerAdmin($username, $nombre, $apellido, $email, $password, $admin) {
-        global $pdo; // Usa la variable global $pdo
-        $user = new Usuario($pdo); // Pasa el objeto PDO al constructor de la clase User
+        global $pdo;
+        $user = new Usuario($pdo);
         return $user->registerAdmin($username, $nombre, $apellido, $email, $password, $admin);
     }
 
     public function recoverPassword($email) {
-        // Enviar enlace de recuperación de contraseña por email
+        // pendiente
     }
 
     public function resetPassword($email, $newPassword) {
-        global $pdo; // Usa la variable global $pdo
-        $user = new User($pdo); // Pasa el objeto PDO al constructor de la clase User
+        global $pdo;
+        $user = new Usuario($pdo);
         return $user->resetPassword($email, $newPassword);
     }
 }
 ?>
-
