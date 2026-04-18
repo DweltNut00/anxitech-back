b4a2149e029c88e5e16569fc165d9c89ee86008b
DweltNut00 <ejhandrade2002@gmail.com>
2026-04-17 20:50:11 -0600

    cors

--- models/Usuario.php
+++ models/Usuario.php
@@ -4,7 +4,7 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
 
-require '..\vendor\autoload.php';
+require __DIR__ . '/../vendor/autoload.php'; 
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
