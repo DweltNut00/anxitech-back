340c4a5111444daebe442df3684c4bd195fa3465
DweltNut00 <ejhandrade2002@gmail.com>
2026-04-24 15:57:08 -0600

    correlaciones

--- api/questions.php
+++ api/questions.php
@@ -22,8 +22,8 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
 
-require_once '..\config\db.php';
-require_once '..\controllers\QuestionController.php';
+require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../controllers/QuestionController.php';
 
 $action = $_GET['action'] ?? '';
 $data = json_decode(file_get_contents('php://input'), true);
