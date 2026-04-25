c4cc080dc866aad278821eb757c9508f0c087401
DweltNut00 <ejhandrade2002@gmail.com>
2026-04-25 08:34:12 -0600

    error

--- controllers/QuestionController.php
+++ controllers/QuestionController.php
@@ -1,7 +1,5 @@
 <?php
 // Incluir la conexión PDO y los modelos Question y Answer
-require_once __DIR__ . '/../config/cors.php';
-require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../models/Question.php';
 
 class QuestionController {
--- controllers/RecommendationController.php
+++ controllers/RecommendationController.php
@@ -1,20 +1,14 @@
 <?php
-// Incluir la conexión PDO y los modelos Recomendacion
-require_once __DIR__ . '/../config/cors.php';
-require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../models/Recomendacion.php';
 
 class RecommendationController {
     private $pdo;
 
-    // Constructor para pasar la conexión PDO
     public function __construct($pdo) {
         $this->pdo = $pdo;
     }
 
-    // Método para generar recomendaciones basadas en las respuestas del usuario
     public function getRecomendacion($resultado) {
-        // Crear una instancia del modelo Question pasando la conexión PDO
         $questionModel = new Recomendacion($this->pdo);
         return $questionModel->getRecomendacion($resultado);
     }
